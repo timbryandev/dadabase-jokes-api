c3c1f1d5f72f638bce2c9d05e13d1ab6d5813634
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-22 16:08:52 +0000

    submit jokes form

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,23 @@
+import type { ActionFunction } from '@remix-run/node'
+import { redirect } from '@remix-run/node'
+
+import { db } from '~/utils/db.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+  const name = form.get('name')
+  const content = form.get('content')
+  const nsfw = form.get('nsfw')
+
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error(`Form not submitted correctly.`)
+  }
+
+  const fields = { name, content, nsfw: nsfw === 'on' }
+  const joke = await db.joke.create({ data: fields })
+  return redirect(`/jokes/${joke.id}`)
+}
+
 export default function NewJokeRoute() {
   return (
     <article>
@@ -5,12 +25,18 @@ export default function NewJokeRoute() {
       <form method='post'>
         <div>
           <label>
-            Name: <input type='text' name='name' />
+            Name: <input type='text' name='name' required />
           </label>
         </div>
         <div>
           <label>
-            Content: <textarea name='content' />
+            Mark as <abbr title='Not Safe For Work'>NSFW</abbr>:{' '}
+            <input type='checkbox' name='nsfw' />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name='content' required />
           </label>
         </div>
         <div>
