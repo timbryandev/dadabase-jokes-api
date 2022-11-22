14966946ace6aa3453cd8955741e90ad7bf32a10
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-22 10:55:31 +0000

    add meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -24,10 +24,26 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang='en'>
+    <html lang='en-GB'>
       <head>
         <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='title' content='Remix Jokes API' />
+        <meta
+          name='description'
+          content='Joke API powered by Remix.js following their tutorial series.'
+        />
+        <meta
+          name='keywords'
+          content='joke, remix, remix.js, react, javascript, jokes, bad jokes, dad jokes'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='language' content='English' />
+        <meta name='author' content='TimBryanDev' />
+
         <title>Remix: So great, it's funny!</title>
+
         <Links />
       </head>
       <body>
