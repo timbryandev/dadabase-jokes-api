e21550ba6a08a860f1768690779d45a216c35961
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-22 15:00:01 +0000

    get joke from api based on url param id

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,30 @@
+import type { LoaderFunction } from '@remix-run/node'
+import type { Joke } from '@prisma/client'
+import { json } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { db } from '~/utils/db.server'
+
+type LoaderData = { joke: Joke }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  })
+
+  if (!joke) throw new Error('Joke not found')
+
+  const data: LoaderData = { joke }
+  return json(data)
+}
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>()
+
   return (
     <div>
-      <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <h2>{data.joke.name}</h2>
+      <p>{data.joke.content}</p>
+      <Link to='.'>Permalink</Link>
     </div>
   )
 }
