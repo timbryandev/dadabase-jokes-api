318f0e877bdb2b373809377637b080085af45973
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-22 11:40:21 +0000

    get jokes from api and display

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,30 +1,77 @@
-import type { LinksFunction } from '@remix-run/node'
-import { Outlet } from '@remix-run/react'
+import type { LinksFunction, LoaderFunction } from '@remix-run/node'
+import type { Joke } from '@prisma/client'
+import { json } from '@remix-run/node'
+import { Link, Outlet, useLoaderData } from '@remix-run/react'
 
+import { db } from '~/utils/db.server'
 import stylesUrl from '~/styles/jokes.css'
 
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesUrl },
-]
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }]
+}
+
+type LoaderData = {
+  jokeListItems: Array<Joke>
+}
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListItems: await db.joke.findMany({
+      orderBy: { name: 'asc' },
+    }),
+  }
+  return json(data)
+}
 
 export default function JokesRoute() {
+  const data = useLoaderData<LoaderData>()
+
+  function renderJokesList() {
+    return data.jokeListItems.map(joke => (
+      <li key={joke.id}>
+        <Link to={joke.id}>{joke.name}</Link>
+      </li>
+    ))
+  }
+
   return (
-    <>
-      <header>
-        <h1>J😝kes</h1>
-        <hr />
+    <div className='jokes-layout'>
+      <header className='jokes-header'>
+        <div className='container'>
+          <h1 className='home-link'>
+            <Link to='/' title='Remix Jokes' aria-label='Remix Jokes'>
+              <span className='logo'>🤪</span>
+              <span className='logo-medium'>J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
       </header>
-      <main>
-        <Outlet />
+      <main className='jokes-main'>
+        <div className='container'>
+          <div className='jokes-list'>
+            <Link to='.'>Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>{renderJokesList()}</ul>
+            <Link to='new' className='button'>
+              Add your own
+            </Link>
+          </div>
+          <div className='jokes-outlet'>
+            <Outlet />
+          </div>
+        </div>
       </main>
-      <footer>
-        <hr />
-        <p>J😝kes</p>
-        <p>
-          Not 100% satisfied? Maybe try the infinitely superior{' '}
-          <a href='https://icanhazdadjoke.com/'>icanhazdadjoke.com</a>!
-        </p>
+      <footer className='jokes-footer'>
+        <div className='container'>
+          <p>
+            Not 100% satisfied? Maybe try the infinitely superior{' '}
+            <a href='https://icanhazdadjoke.com/'>icanhazdadjoke.com</a>!
+          </p>
+          <p>
+            Developed by <a href='https://timbryan.dev'>Tim Bryan.dev</a>.
+          </p>
+        </div>
       </footer>
-    </>
+    </div>
   )
 }
