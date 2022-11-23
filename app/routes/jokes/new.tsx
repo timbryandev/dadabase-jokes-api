767712a839694dca83406467c629418c121730e7
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-23 10:02:06 +0000

    require userId to submit a new joke

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -4,6 +4,8 @@ import { useActionData } from '@remix-run/react'
 
 import { db } from '~/utils/db.server'
 
+import { requireUserId } from '~/utils/session.server'
+
 function validateJokeContent(content: string) {
   if (content.length < 10) {
     return `That joke is too short`
@@ -33,7 +35,7 @@ const badRequest = (data: ActionData) => json(data, { status: 400 })
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData()
-
+  const userId = await requireUserId(request)
   const name = form.get('name')
   const content = form.get('content')
   const nsfw = form.get('nsfw')
@@ -55,7 +57,10 @@ export const action: ActionFunction = async ({ request }) => {
     return badRequest({ fieldErrors, fields })
   }
 
-  const joke = await db.joke.create({ data: fields })
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId },
+  })
+
   return redirect(`/jokes/${joke.id}`)
 }
 
