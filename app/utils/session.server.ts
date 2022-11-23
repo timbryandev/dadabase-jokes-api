a35b0160a35272cb4edba0e96781b1c7121bfd68
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-23 10:01:50 +0000

    user session helpers

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -55,3 +55,32 @@ export async function createUserSession(userId: string, redirectTo: string) {
     },
   })
 }
+
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get('Cookie'))
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+
+  if (!userId || typeof userId !== 'string') return null
+
+  return userId
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname,
+) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+
+  if (!userId || typeof userId !== 'string') {
+    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+
+    throw redirect(`/login?${searchParams}`)
+  }
+
+  return userId
+}
