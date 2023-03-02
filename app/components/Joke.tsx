7c6fc525b25d21f066f639eb8230ff9fb458ad8d
TimBryanDev <hi@timbryan.dev>
2023-03-02 20:29:50 +0000

    convert p tag to blockquote

--- app/components/Joke.tsx
+++ app/components/Joke.tsx
@@ -13,7 +13,7 @@ export default function Joke({ content, id, jokester, name }: JokeProps) {
       <header>
         <h2>{name}</h2>
       </header>
-      <p>{content}</p>
+      <blockquote className="joke-content">{content}</blockquote>
       {typeof jokester === 'string' && <p>Credit: {jokester}</p>}
       <Link to={`/jokes/${id}`}>Permalink for "{name}"</Link>
     </section>
