2d688323bd43d6e54500d2555003494ccb8936c7
TimBryanDev <hi@timbryan.dev>
2023-06-01 23:31:39 +0100

    :lipstick: replace list bullet with new joke icon

---
+++ app/components/WarningNew.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function WarningNew() {
+  return (
+    <span
+      className='warning warning__new'
+      title='This joke is fresh of the press and at the top of the jokes list to make sure you see any other newly added jokes'
+    >
+      🆕
+    </span>
+  )
+}
