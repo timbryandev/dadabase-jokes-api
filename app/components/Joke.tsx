87951579e92cdec74854d205b8e50c8171e79cb2
TimBryanDev <hi@timbryan.dev>
2023-06-01 22:55:28 +0100

    :sparkles: show loading bar for loading jokes

--- app/components/Joke.tsx
+++ app/components/Joke.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from '@remix-run/react'
+import { Link, useTransition } from '@remix-run/react'
 import WarningNSFW from '~/components/WarningNSFW'
 
 type JokeProps = {
@@ -11,8 +11,20 @@ type JokeProps = {
 }
 
 export default function Joke({ content, id, jokester, name, nsfw }: JokeProps) {
+  const { state } = useTransition()
+
   return (
     <section className='joke'>
+      {state === 'loading' && (
+        <progress
+          style={{
+            position: 'absolute',
+            top: '-1rem',
+            left: '0',
+            width: '100%',
+          }}
+        />
+      )}
       <header>
         <h2>
           {nsfw && <WarningNSFW />} {name}
